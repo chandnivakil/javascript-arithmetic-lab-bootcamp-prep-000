a5fee01350339c9b5811835a20ab2c84ec439623
fantastic-page-5704 <chandvakil@gmail.com>
2018-11-19 21:19:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,20 +14,5 @@ function multiply (a,b) {
   return a * b
 }
 
-var number = 10 
-
-function add5() {
-  number += 5
-}
-
-function divideBy3() {
-  number /= 3
-}
-
-divideBy3()
-console.log(number)
-
-number = 10
-
-
+var n = 5
 n++
